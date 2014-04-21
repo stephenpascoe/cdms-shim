a02ef0ef5c7013026e35e37a0ae4115495c69a49
Stephen Pascoe <Stephen.Pascoe@stfc.ac.uk>
2014-04-21 21:51:22 +0100

    Translated Cdunif.py into an abstract interface.

--- src/cdms2/Cdunif_abc.py
+++ src/cdms2/Cdunif_abc.py
@@ -3,47 +3,59 @@ The Cdunif interface translated into Python
 
 """
 
-class CdunifFile(object):
+from abc import ABCMeta, abstractmethod, abstractproperty
 
+class AbstractCdunifFile(object):
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
     def __init__(self, filename, node, history=None):
         if history is not None:
             raise NotImplementedError('cdunif-shim does not support adding history on file creation')
 
-    @property
+    @abstractproperty
     def attributes(self):
         raise NotImplementedError
 
-    @property
+    @abstractproperty
     def variables(self):
         raise NotImplementedError
 
-    @property
+    @abstractproperty
     def dimensioninfo(self):
         raise NotImplementedError
 
     #!FIXME: How to emulate attributes in __dict__
 
+    @abstractmethod
     def __getattr__(self, attr):
         raise NotImplementedError
 
+    @abstractmethod
     def __setattr__(self, attr, value):
         raise NotImplementedError
 
+    @abstractmethod
     def __repr__(self):
         raise NotImplementedError
 
+    @abstractmethod
     def close(self):
         raise NotImplementedError
 
+    @abstractmethod
     def createDimension(self, name, length):
         raise NotImplementedError
 
+    @abstractmethod
     def createVariable(self, name, typecode, dimensions):
         raise NotImplementedError
 
+    @abstractmethod
     def readDimension(self, name):
         raise NotImplementedError
 
+    @abstractmethod
     def sync(self):
         raise NotImplementedError
 
@@ -51,37 +63,42 @@ class CdunifFile(object):
         return self.sync()
 
 
-class CdunifVariable(object):
+class AbstractCdunifVariable(object):
 
-    @property
+    @abstractproperty
     def shape(self):
         raise NotImplementedError
 
-    @property
+    @abstractproperty
     def dimensions(self):
         raise NotImplementedError
 
     #!FIXME: how to emulate attributes in __dict__
 
+    @abstractmethod
     def __getattr__(self, attr):
         raise NotImplementedError
 
+    @abstractmethod
     def __setattr__(self, attr, value):
         raise NotImplementedError
 
+    @abstractmethod
     def __len__(self):
         raise NotImplementedError
 
+    @abstractmethod
     def __getitem__(self, key):
         raise NotImplementedError
 
+    @abstractmethod
     def __setitem__(self, key, value):
         raise NotImplementedError
 
 
-def CdunifSetNCFLAGS():
-    pass
-
-def CdunifGetNCFLAGS():
-    pass
+#def CdunifSetNCFLAGS():
+#    pass
+#
+#def CdunifGetNCFLAGS():
+#    pass
 
