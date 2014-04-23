093306d38ef5930f5e525f22d0cb884930c336c5
Stephen Pascoe <Stephen.Pascoe@stfc.ac.uk>
2014-04-23 14:57:27 +0100

    Working replacement for _bindex

--- bindex_api.py
+++ bindex_api.py
@@ -23,35 +23,35 @@ def bindex(lats, lons):
         an array of shape (720 * 360, ) containing indexes of `head` for the next grid point
 
     """
-    #!TODO: replace this slow implementation with a numpy-optimised one
-
-
-    head = np.ones((BINLEN, ), np.int) * -1
-    last = np.ones((BINLEN, ), np.int) * -1
-    next = np.ones((BINLEN, ), np.int) * -1
-
+def bindexHorizontalGrid(lats, lons):
     assert len(lats.shape) == 1
     n = lats.shape[0]
 
+    i = np.floor(lons/XBINI).astype(np.int) % NBINI
+    i = np.where(i < 0, i + NBINI, i)
+
+    j = np.floor((lats + 90.0) / XBINJ).astype(np.int)
+    mj = np.where(j > 0, j, 0)
+    j = np.where(mj > NBINJ - 1, NBINJ - 1, mj)
+
+    oind = NBINJ*i + j
+
+    head = np.ones(BINLEN, np.int) * -1
+    last = np.ones(BINLEN, np.int) * -1
+    next = np.ones(n, np.int) * -1
+
+    #!FIXME: this is slow but making it faster would probably require Cython 
+    #        or the original bindex code
     for ireg in xrange(n):
-        i = int(np.floor(lons[ireg]/XBINI))
-        i = i % NBINI
-
-        if i < 0: i += NBINI
-
-        j = int(np.floor((lats[ireg]+90.0)/XBINJ))
-        j = min(max(j, 0), NBINJ - 1)
-
-        oind = NBINJ*i + j
-
-        if head[oind] == -1:
-            head[oind] = last[oind] = ireg
+        oind_i = oind[ireg]
+        if head[oind_i] == -1:
+            head[oind_i] = last[oind_i] = ireg
         else:
-            next[last[oind]] = ireg
-            last[oind] = ireg
+            next[last[oind_i]] = ireg
+            last[oind_i] = ireg
+
 
     return (head, next)
-
 
 def intersect(slat, slon, elat, elon, lats, lons, head, 
               next, points, latind, lonind):
@@ -60,9 +60,108 @@ def intersect(slat, slon, elat, elon, lats, lons, head,
     raise NotImplementedError
 
 
-if __name__ == '__main__':
-    lats = np.arange(-90, 90, 0.5)
-    lons = np.arange(0, 360, 0.5)
-    X, Y = np.meshgrid(lons, lats)
+def intersect(slat, slon, elat, elon, lats, lons,
+              head, next, points, latind, lonind):
+    npoints = 0
+    ngrid = len(lats)
+    
 
-    head, next = bindex(Y, X)
+    schunk = int(slon/360.0)
+    echunk = int(elon/360.0)
+    xslon = slon - 360.0 * schunk
+    xelon = elon - 360.0 * echunk
+    if schunk == echunk:
+        npoints = intersect_l(slat, xslon, elat, xelon, lats, lons, 
+                              head, ngrid, next, points, npoints, latind, lonind)
+    else:
+        loni = '%so' % lonind[0]
+        npoints = intersect_l(slat, xslon, elat, 360.0, lats, lons, 
+                              head, ngrid, next, points, npoints, latind, loni)
+        loni = 'c%s' % lonind[1]
+        npoints = intersect_l(slat, 0.0, elat, xelon, lats, lons, 
+                              head, ngrid, next, points, npoints, latind, loni)
+
+    return npoints
+
+def intersect_l(slat, slon, elat, elon, lats, lons, head, ngrid, next, points, npoints, latind, lonind):
+
+    si = int(slon/XBINI)
+    si = min(max(si,0),(NBINI-1))
+    sj = int((slat+90.0)/XBINI)
+    sj = min(max(sj,0),NBINJ-1)
+    ei = int(elon/XBINI)
+    ei = min(max(ei,0),(NBINI-1))
+    ej = int((elat+90.0)/XBINI)
+    ej = min(max(ej,0),NBINJ-1)
+
+    latind0 = latind[0]
+    latind1 = latind[1]
+    lonind0 = lonind[0]
+    lonind1 = lonind[1]
+
+    if slon == elon and (lonind0 == 'o' or lonind1 == 'o'):
+        return npoints
+
+    for i in xrange(si, ei+1):
+        for j in xrange(sj, ej+1):
+            hind = NBINJ*i + j
+            rind = head[hind]
+            if i == si or i == ei or j == sj or j == ej:
+                while rind != -1:
+                    lat = lats[rind]
+                    lon = lons[rind]
+                    if (((latind0=='c' and slat<=lat) or (latind0=='o' and slat<lat)) and
+			((latind1=='c' and lat<=elat) or (latind1=='o' and lat<elat)) and
+			((lonind0=='c' and slon<=lon) or (lonind0=='o' and slon<lon)) and
+			((lonind1=='c' and lon<=elon) or (lonind1=='o' and lon<elon))):
+                        if npoints == ngrid:
+                            print 'Internal error in intersect.'
+                            return npoints
+                        points[npoints] = rind
+                        npoints += 1
+                    rind = next[rind]
+            else:
+                while rind != -1:
+                    if npoints == ngrid:
+                        print 'Internal error in intersect.'
+                        return npoints
+                    points[npoints] = rind
+                    npoints += 1
+                    rind = next[rind]
+                    
+    return npoints
+
+if __name__ == '__main__':
+    import cdms2.bindex
+    
+    lats = np.arange(-90, 90, 2.5)
+    lons = np.arange(0, 360, 1.5)
+    X, Y = np.meshgrid(lons, lats)
+    
+    Xr, Yr = X.ravel(), Y.ravel()
+    head, next = cdms2.bindex.bindexHorizontalGrid(Yr, Xr)
+    head2, next2 = bindexHorizontalGrid(Yr, Xr)
+
+    print (head - head2 == 0).all()
+    print (next - next2 == 0).all()
+
+    slon = 270.0
+    elon = 380.0
+    lonopt = 'oc'
+
+    slat = -45.0
+    elat = 90.0
+    latopt = 'cc'
+
+    points = np.zeros(len(Yr), np.int)
+    npoints = cdms2.bindex._bindex.intersect(slat, slon, elat, elon, Yr, Xr, head2, next2,
+                                             points, latopt, lonopt)
+
+    points2 = np.zeros(len(Yr), np.int)
+    npoints2 = intersect(slat, slon, elat, elon, Yr, Xr, head2, next2,
+                                             points, latopt, lonopt)
+
+    print npoints == npoints2
+    print (points - points == 0).all()
+
+    print npoints, npoints2
